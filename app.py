da41b984fb0e8d3ad213802b2887d6bc8b656d4a
Felix <62290842+ctrl-Felix@users.noreply.github.com>
2024-10-19 01:55:46 +0200

    allow CORS

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from litestar import Litestar, get
+from litestar.config.cors import CORSConfig
+
 from who_is import whois as whois_lookup
 
 from models.whois_response import WhoisResponse
@@ -9,5 +11,6 @@ async def whois(domain: str) -> WhoisResponse:
     result = whois_lookup(domain)
     return WhoisResponse(**result.json(), raw=result.raw)
 
+cors_config = CORSConfig(allow_origins=["*"])
 
-app = Litestar([whois])
+app = Litestar([whois], cors_config=cors_config)
