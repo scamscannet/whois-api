094ebe3fc571ba0ae0f25441921432ca15dfb305
felix <Felix Pfeiffer>
2025-01-05 23:04:15 +0100

    update docs

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from litestar import Litestar, get
 from litestar.config.cors import CORSConfig
+from litestar.openapi import OpenAPIConfig
+from litestar.response import Redirect
+from litestar.status_codes import HTTP_301_MOVED_PERMANENTLY
 
 from who_is import whois as whois_lookup
 
@@ -11,6 +14,16 @@ async def whois(domain: str) -> WhoisResponse:
     result = whois_lookup(domain)
     return WhoisResponse(**result.json(), raw=result.raw)
 
+@get("/", status_code=HTTP_301_MOVED_PERMANENTLY)
+async def redirect_to_docs() -> Redirect:
+    return Redirect(path="/schema/elements")
+
 cors_config = CORSConfig(allow_origins=["*"])
 
-app = Litestar([whois], cors_config=cors_config)
+openapi_config = OpenAPIConfig(
+    title="Octobyte Whois API",
+    description="REST API providing Whois lookups for domains and IPs",
+    version="0.1",
+
+)
+app = Litestar([whois], cors_config=cors_config, openapi_config=openapi_config)
